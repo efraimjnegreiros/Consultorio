5b930c13c4bce9ec317ab4a016fd5d42d9110fb7
Mateus juan <169060996+MateusJuan@users.noreply.github.com>
2024-11-20 17:30:43 -0300

    .

---
+++ scr/cadastroidentificacao/Medico.java
@@ -0,0 +1,45 @@
+public class Medico extends Consultorio {
+    private String crmUf;
+    private String especialidade;
+    private String areaAtuacao;
+
+    public Medico(String nome, String cpf, int idade, String emailPessoal, String endereco, 
+                  String crmUf, String especialidade, String areaAtuacao) {
+        super(nome, cpf, idade, emailPessoal, endereco);
+        this.crmUf = crmUf;
+        this.especialidade = especialidade;
+        this.areaAtuacao = areaAtuacao;
+    }
+
+    public String getCrmUf() {
+        return this.crmUf;
+    }
+
+    public void setCrmUf(String crmUf) {
+        this.crmUf = crmUf;
+    }
+
+    public String getEspecialidade() {
+        return this.especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public String getAreaAtuacao() {
+        return this.areaAtuacao;
+    }
+
+    public void setAreaAtuacao(String areaAtuacao) {
+        this.areaAtuacao = areaAtuacao;
+    }
+
+    public void setTelefone(String telefone) {
+        super.setTelefone(telefone);
+    }
+
+    public void consulta(Paciente paciente, Medico medico) {
+        System.out.println("O médico " + this.getNome() + " está atendendo o paciente " + paciente.getNome());
+    }
+}
