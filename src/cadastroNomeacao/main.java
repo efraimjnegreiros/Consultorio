ebeced5ebbb0713ed905049790073011b19bab28
Mateus juan <169060996+MateusJuan@users.noreply.github.com>
2024-11-24 11:21:33 -0300

    24/11/2024

---
+++ src/cadastroNomeacao/main.java
@@ -0,0 +1,11 @@
+package cadastroNomeacao;
+
+public class Main {
+    public static void main(String[] args) {
+        Medico medico = new Medico("Dr. João", "123.456.789-00", 40, "joao@exemplo.com", "Rua A, 123", "CRM1234", "Cardiologista", "Hospital X");
+        Paciente paciente = new Paciente("123456789", "Hipertensão", 2.5f, true, "Carlos", "987.654.321-00", 55, "carlos@gmail.com", "Rua B, 456", "1234-5678");
+        
+        System.out.println("Médico: " + medico.getNome() + ", Especialidade: " + medico.getEspecialidade());
+        System.out.println("Paciente: " + paciente.getNome() + ", Doença: " + paciente.getDoenca());
+    }
+}
