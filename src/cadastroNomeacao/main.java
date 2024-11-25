74c976fa16cd3a7ea2faa1576d3d7e851d096674
Mateus juan <169060996+MateusJuan@users.noreply.github.com>
2024-11-25 11:50:26 -0300

    25/11/2024

--- src/cadastroNomeacao/main.java
+++ src/cadastroNomeacao/main.java
@@ -2,58 +2,57 @@ package cadastroNomeacao;
 
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner scanner = new scanner(System.in);
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("=== Cadastro do Médico ===");
         Medico medico = new Medico(
-            input("Nome" , scanner),
-            input("CPF" , scanner),
-            integer.parseInt(input("Idade",scanner)),
-            
-            input("Email",scanner),
-            input("Endereço",scanner),
-            input("CRM/UF",scanner),
-            input("Espeialidade",scanner),
+            input("Nome", scanner),
+            input("CPF", scanner),
+            Integer.parseInt(input("Idade", scanner)),
+            input("Email", scanner),
+            input("Endereço", scanner),
+            input("CRM/UF", scanner),
+            input("Especialidade", scanner),
             input("Área de atuação", scanner)
         );
 
         System.out.println("\n=== Cadastro do Paciente ===");
         Paciente paciente = new Paciente(
             input("Número CNS", scanner),
-            input("Doença",scanner),
-            Float.parseInt(input("Tempo de doença(em anos)",scanner))
-            Boolean.parseBoolean(input("DIreito a teleconsulta (true/false)",scanner)),
-            input("Nome",scanner),
-            input("CPF",scanner),
-            integer.parseInt(input("Idade",scanner)),
-            input("Email",scanner),
-            input("Endereço",scanner),
-            input("Telefone",scanner)
+            input("Doença", scanner),
+            Float.parseFloat(input("Tempo de doença(em anos)", scanner)),
+            Boolean.parseBoolean(input("Direito a teleconsulta (true/false)", scanner)),
+            input("Nome", scanner),
+            input("CPF", scanner),
+            Integer.parseInt(input("Idade", scanner)),
+            input("Email", scanner),
+            input("Endereço", scanner),
+            input("Telefone", scanner)
         );
 
         System.out.println("\n=== Cadastro do Atendente ===");
         Atendente atendente = new Atendente(
-            integer.parseInt(input("Número de identificação", scanner)),
+            Integer.parseInt(input("Número de identificação", scanner)),
             Boolean.parseBoolean(input("Home office (true/false)", scanner)),
-            input("Nome",scanner),
-            input("CPF",scanner),
-            integer.parseInt(input("Idade",scanner)),
-            input("Email",scanner),
-            input("Endereco",scanner),
-            input("Telefone",scanner)
+            input("Nome", scanner),
+            input("CPF", scanner),
+            Integer.parseInt(input("Idade", scanner)),
+            input("Email", scanner),
+            input("Endereço", scanner),
+            input("Telefone", scanner)
         );
 
         System.out.println("\n=== Resultados ===");
         medico.consulta(paciente);
-        atendente.consulta(paciente,medico);
+        atendente.consulta(paciente, medico);
 
-        scanner.closse();
+        scanner.close();
+    }
 
-        private static String input(String prompt, Scanner scanner){
-            System.out.println(prompt + ": ");
-            return scanner.nextLine();
-        }
+    private static String input(String prompt, Scanner scanner) {
+        System.out.println(prompt + ": ");
+        return scanner.nextLine();
     }
 }
