382c34d8b921fb0edf0dbd695759d0b62c6325ce
Efraim Negreiros <efraimjnegreiross@gmail.com>
2024-12-02 12:31:28 -0300

    Update Formacao.java

--- src/cadastroNomeacao/Formacao.java
+++ src/cadastroNomeacao/Formacao.java
@@ -1,5 +1,19 @@
 package cadastroNomeacao;
 
 public class Formacao {
-    
+    private String universidade;
+    private String curso;
+    private boolean apresentouArtigo;
+
+
+    public Formacao(String universidade, String curso, boolean apresentouArtigo) {
+        this.universidade = universidade;
+        this.curso = curso;
+        this.apresentouArtigo = apresentouArtigo;
+    }
+    public void printDadosAcademicos() {
+        System.out.println("Universidade: " + this.universidade);
+        System.out.println("Curso: " + this.curso);
+        System.out.println("Apresentou artigo? " + this.apresentouArtigo);
+    }
 }
