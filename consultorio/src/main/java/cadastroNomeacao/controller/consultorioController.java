2946324b21573caed2d4945e428dab178322ec41
Mateus juan <169060996+MateusJuan@users.noreply.github.com>
2025-01-29 08:54:38 -0300

    izgodosottzos

--- consultorio/src/main/java/cadastroNomeacao/controller/consultorioController.java
+++ consultorio/src/main/java/cadastroNomeacao/controller/consultorioController.java
@@ -1,15 +1,31 @@
 package cadastroNomeacao.controller;
 
-
-
+import cadastroNomeacao.Paciente;
+import cadastroNomeacao.service.PacienteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.spring.framework.web.bin.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-@controller
-public class PacienteController{
+@Controller
+public class PacienteController {
+
+    private final PacienteService pacienteService;
+
+    public PacienteController(PacienteService pacienteService) {
+        this.pacienteService = pacienteService;
+    }
+
     @GetMapping("/cad_paciente")
-    public String cadastrarPaciente(){
-        return "cadastro_paciente"
+    public String exibirFormularioCadastroPaciente(Model model) {
+        model.addAttribute("paciente", new Paciente());
+        return "cadastro_paciente"; // Nome do arquivo HTML
+    }
+
+    @PostMapping("/cad_paciente")
+    public String salvarPaciente(@ModelAttribute("paciente") Paciente paciente) {
+        pacienteService.salvarPaciente(paciente);
+        return "redirect:/cad_paciente";
     }
 }
